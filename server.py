5c90e5a94f5ed95686397ab8783cdc06153a369b
Newnius <newnius.cn@gmail.com>
2019-03-20 10:48:28 +0800

    bugfix

--- server.py
+++ server.py
@@ -24,25 +24,24 @@ class MyHandler(BaseHTTPRequestHandler):
 			self.wfile.write(bytes("pong", "utf-8"))
 
 		elif req.path == "/logs":
-			id = query['id'][0]
 			try:
+				container_id = query['id'][0]
 				client = docker.from_env()
-				container = client.containers.get(id)
+				container = client.containers.get(container_id)
 				msg = {'code': 0, 'logs': str(container.logs().decode())}
 			except Exception as e:
-				msg = {'code': 0, 'error': e}
+				msg = {'code': 0, 'error': str(e)}
 			self.send_response(200)
 			self.send_header('Content-type', 'application/json')
 			self.end_headers()
 			self.wfile.write(bytes(json.dumps(msg), "utf-8"))
 
 		elif req.path == "/status":
-			id = query['id'][0]
+			container_id = query['id'][0]
 			client = docker.from_env()
-			container = client.containers.list(all=True, filters={'id': id})
+			container = client.containers.list(all=True, filters={'id': container_id})
 			if len(container) > 0:
 				container = container[0]
-				print(container.image.attrs)
 				status = {
 					'id': container.short_id,
 					'image': container.attrs['Config']['Image'],
@@ -65,68 +64,67 @@ class MyHandler(BaseHTTPRequestHandler):
 		else:
 			self.send_error(404, 'File Not Found: %s' % self.path)
 
+	# Handler for the POST requests
+	def do_POST(self):
+		if self.path == "/create":
+			form = cgi.FieldStorage(
+				fp=self.rfile,
+				headers=self.headers,
+				environ={
+					'REQUEST_METHOD': 'POST',
+					'CONTENT_TYPE': self.headers['Content-Type'],
+				})
+			docker_image = form["image"].value
+			docker_cmd = form["cmd"].value
 
-# Handler for the POST requests
-def do_POST(self):
-	if self.path == "/create":
-		form = cgi.FieldStorage(
-			fp=self.rfile,
-			headers=self.headers,
-			environ={
-				'REQUEST_METHOD': 'POST',
-				'CONTENT_TYPE': self.headers['Content-Type'],
-			})
-		docker_image = form["image"].value
-		docker_cmd = form["cmd"].value
+			try:
+				client = docker.from_env()
+				container = client.containers.run(
+					image=docker_image,
+					command=docker_cmd,
+					environment={"key": "value"},
+					runtime="nvidia",
+					detach=True
+				)
+				msg = {"code": 0, "id": container.id}
+			except Exception as e:
+				msg = {"code": 1, "error": str(e)}
 
-		try:
+			self.send_response(200)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
+			self.wfile.write(bytes(json.dumps(msg), "utf-8"))
+
+		elif self.path == "/stop":
+			form = cgi.FieldStorage(
+				fp=self.rfile,
+				headers=self.headers,
+				environ={
+					'REQUEST_METHOD': 'POST',
+					'CONTENT_TYPE': self.headers['Content-Type'],
+				})
+			container_id = form["id"].value
+
 			client = docker.from_env()
-			container = client.containers.run(
-				image=docker_image,
-				command=docker_cmd,
-				environment={"key": "value"},
-				runtime="nvidia",
-				detach=True
-			)
-			msg = {"code": 0, "id": container.id}
-		except Exception as e:
-			msg = {"code": 1, "error": e}
+			container = client.containers.get(container_id)
+			container.stop()
+			msg = {"code": 0}
 
-		self.send_response(200)
-		self.send_header('Content-type', 'application/json')
-		self.end_headers()
-		self.wfile.write(bytes(json.dumps(msg), "utf-8"))
-
-	elif self.path == "/stop":
-		form = cgi.FieldStorage(
-			fp=self.rfile,
-			headers=self.headers,
-			environ={
-				'REQUEST_METHOD': 'POST',
-				'CONTENT_TYPE': self.headers['Content-Type'],
-			})
-		id = form["id"].value
-
-		client = docker.from_env()
-		container = client.containers.get(id)
-		container.stop()
-		msg = {"code": 0}
-
-		self.send_response(200)
-		self.send_header('Content-type', 'application/json')
-		self.end_headers()
-		self.wfile.write(bytes(json.dumps(msg), "utf-8"))
-	else:
-		self.send_error(404, 'File Not Found: %s' % self.path)
+			self.send_response(200)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
+			self.wfile.write(bytes(json.dumps(msg), "utf-8"))
+		else:
+			self.send_error(404, 'File Not Found: %s' % self.path)
 
 
 try:
 	# Create a web server and define the handler to manage the
 	# incoming request
 	server = HTTPServer(('', PORT_NUMBER), MyHandler)
-	print('Started httpserver on port ', PORT_NUMBER)
+	print('Started http server on port ', PORT_NUMBER)
 
-	# Wait forever for incoming htto requests
+	# Wait forever for incoming http requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
